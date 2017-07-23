8482801c510cd38ee775adbc98c23f38c868ee4e
Denis Souza <denyssouza1@hotmail.com>
2017-07-23 16:33:50 -0300

    layout formulário novo desafio, ainda sem validações de campos

--- app/src/main/java/br/com/clubeapp/a52semanas/Activitys/Activitys/DesafiosActivity.java
+++ app/src/main/java/br/com/clubeapp/a52semanas/Activitys/Activitys/DesafiosActivity.java
@@ -1,20 +1,18 @@
 package br.com.clubeapp.a52semanas.Activitys.Activitys;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yarolegovich.lovelydialog.LovelyCustomDialog;
 
 import java.util.ArrayList;
 
@@ -54,9 +52,53 @@ public class DesafiosActivity extends AppCompatActivity {
     }
 
     public void NovoDesafio(){
-        intent = new Intent(this,NovoDesafioActivity.class);
+        /*new LovelyTextInputDialog(this, R.style.TintTheme)
+                .setTopColorRes(R.color.colorPrimary)
+                .setTitle(R.string.novo_desafio)
+                .setHint(R.string.action_settings)
+                .setInputFilter(R.string.app_name, new LovelyTextInputDialog.TextFilter() {
+                    @Override
+                    public boolean check(String text) {
+                        return text.matches("\\w+");
+                    }
+                })
+                .setIcon(R.drawable.ic_date)
+                .setConfirmButton(android.R.string.ok, new LovelyTextInputDialog.OnTextInputConfirmListener() {
+                    @Override
+                    public void onTextInputConfirmed(String text) {
+                        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(android.R.string.no,null)
+                .show();*/
+
+       /* new LovelyCustomDialog(this)
+                .setView(R.layout.form_novo_desafio)
+                .setTopTitle(R.string.novo_desafio)
+                .setCancelable(false)
+                .setTopTitleColor(R.color.white)
+                .setTopColorRes(R.color.colorPrimary)
+                .setIcon(R.drawable.ic_add)
+                .setListener(R.id.btn_ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .setListener(R.id.btn_cancel,new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .show();*/
+
+       Intent intent = new Intent(this,NovoDesafioActivity.class);
         startActivity(intent);
+
     }
+
     public void configRecyclerView(){
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
--- app/src/main/java/br/com/clubeapp/a52semanas/Activitys/Activitys/NovoDesafioActivity.java
+++ app/src/main/java/br/com/clubeapp/a52semanas/Activitys/Activitys/NovoDesafioActivity.java
@@ -1,25 +1,47 @@
 package br.com.clubeapp.a52semanas.Activitys.Activitys;
 
-import android.support.v4.content.ContextCompat;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
 
 import br.com.clubeapp.a52semanas.R;
 
-public class NovoDesafioActivity extends AppCompatActivity {
+public class NovoDesafioActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,
+                                                                        DialogInterface.OnCancelListener {
 
-    Toolbar toolbar;
+    private int year,month,day;
+    private  EditText dataInicial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novo_desafio);
+        setContentView(R.layout.form_novo_desafio);
 
-       getSupportActionBar().setTitle("Novo Desafio");
+        dataInicial= (EditText)findViewById(R.id.data_inicial);
+        dataInicial.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    openDatePicker(v);
+                }
+            }
+        });
+
+        dataInicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(v);
+            }
+        });
+        getSupportActionBar().setTitle("Novo Desafio");
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -32,4 +54,52 @@ public class NovoDesafioActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void openDatePicker(View view){
+        initDatePicker();
+        Calendar cDefault = Calendar.getInstance();
+
+
+        cDefault.set(year,month,day);
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
+                this,
+                cDefault.get(Calendar.YEAR),
+                cDefault.get(Calendar.MONTH),
+                cDefault.get(Calendar.DAY_OF_MONTH)
+        );
+
+        Calendar dataMin = Calendar.getInstance();
+        datePickerDialog.setMinDate(dataMin);
+        datePickerDialog.setOnCancelListener(this);
+        datePickerDialog.show(getFragmentManager(),"DatePickerDialog");
+    }
+
+    private void initDatePicker(){
+        if (year == 0){
+            Calendar c = Calendar.getInstance();
+            year = c.get(Calendar.YEAR);
+            month= c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar tDefault = Calendar.getInstance();
+        tDefault.set(this.year,this.month,this.day);
+
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+
+        dataInicial.setText((day<10?"0"+day:day)+"/"+
+                            (month+1<10?"0"+(month+1):month+1)+"/"+
+                            (year<10?"0"+year:year)
+                            );
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialogInterface) {
+        year = month=day=0;
+        dataInicial.setText("");
+    }
 }
